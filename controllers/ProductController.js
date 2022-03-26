3eedfcd6ee7007c91cc0ee6d34738297594c9284
MoetazBrayekit <moetaz.brayek@aigot.com>
2022-03-27 12:21:26 +1300

    :penguin: Product Crud

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -9,63 +9,47 @@ import Joi from 'joi';
  * @param {Object} req The object that represents the http call that called the api endpoint
  * @param {Object} res The object that represents the response to the http call
  */
- export const createProduct = asyncHandler(async (req,res)=>{
-    console.log("hi");
+ export const CreateProduct = asyncHandler(async (req,res)=>{     
     const schema = Joi.object({
         name: Joi.string().required(),
         productImage: Joi.string().required(),
         price: Joi.number().required(),
-        category: Joi.string().required(),
         countInStock: Joi.number().required(),
         description: Joi.string().required(),
         averageRating: Joi.number().required()
     });
-    //if (schema['error'] !== undefined) throw new Error(validation.error);
+    const validation = schema.validate({ 
+         name: req.body.name,
+         productImage: req.body.productImage,
+         price: req.body.price,
+         countInStock: req.body.countInStock,
+         description: req.body.description,
+         averageRating: req.body.averageRating,
+         });
+    if (validation['error'] !== undefined) throw new Error(validation.error);
     const product = await Product.create({...req.body});
     res.status(200).send({status: "Product Added Successfully", data: product});
 
 });
 
 
-export const getProducts = asyncHandler(async (req,res)=>{
-
-
-    const keyWord = req.query.keyWord;
-    const ltORgt = req.query;
-    if(keyWord || ltORgt ){
-        const searchItem = keyWord ?
-            {name : {$regex: keyWord , $options: "i"}}:
-            {};
-        const priceRange = (!isNaN(Number(ltORgt.priceMin)) && !isNaN(Number(ltORgt.priceMax))) ?
-            {price: {$gt: Number(ltORgt.priceMin), $lt: Number(ltORgt.priceMax)}}:
-            {};
-        let BrandQuery =  req.query.Brand.split(",")
-        let CategoryQuery =  req.query.Category.split(",")
-
-        const BrandSearch = req.query.Brand ? {brand : {$in : BrandQuery }}  : {}
-        const CategorySearch = req.query.Category ? {category : {$in : CategoryQuery }}  : {}
-
-           const searchProduct = await Product.find({$and :[ priceRange , BrandSearch,CategorySearch ]}).populate('Category').populate('brand');
-
-        res.status(200).send({
-            status: "success",
-            data: { results : searchProduct.reverse() , count: searchProduct.length }
-        })
-    }else{
-        const products = await Product.find().populate('brand').populate('Category');
-
-        res.status(200).send({
-            status: "success",
-            data: { results : products.reverse() , count: products.length }
-        })
-    }
+export const GetProducts = asyncHandler(async (req,res)=>{
+    let product = await Product.find();
+    if(!product)
+    throw createError(404,`Product with id ${req.params.productId} not found`);
+    res.status(200).send({status : "success",data : product});
 
 });
-export const getProduct =asyncHandler(async (req,res)=>{
-    const product= await Product.findById(req.params.productId).populate({
-        path : "Review",
-        select: "title text"
-    }).populate('Category').populate('brand');
+export const GetProduct =asyncHandler(async (req,res)=>{
+    const product= await Product.findById(req.params.productId);
+    if(!product)
+        throw createError(404,`Product with id ${req.params.productId} not found`);
+
+    res.status(200).send({status : "success",data : product});
+})
+
+export const RemoveProduct =asyncHandler(async (req,res)=>{
+    const product= await Product.findOneAndDelete(req.params.productId);
     if(!product)
         throw createError(404,`Product with id ${req.params.productId} not found`);
 
@@ -75,14 +59,8 @@ export const getProduct =asyncHandler(async (req,res)=>{
 
 
 
-
-
-export const updateProduct = asyncHandler(async (req,res,next)=>{
-  const  productToUpdate =req.body;
-    productToUpdate.color = JSON.parse(req.body.color);
-    // productToUpdate.size = JSON.parse(req.body.size);
-    // console.log(productToUpdate.color)
-
+export const UpdateProduct = asyncHandler(async (req,res,next)=>{
+    let  productToUpdate =req.body;
     const editProduct = await Product.findByIdAndUpdate(
         req.params.productId,
         productToUpdate,
@@ -94,18 +72,7 @@ export const updateProduct = asyncHandler(async (req,res,next)=>{
 
     if(!editProduct)
         throw createError(404 , `Product with id ${req.params.productId} not found `);
-
     const updatedProduct = await Product.findById(req.params.productId);
-
     res.status(201).send({status : "success" , data : updatedProduct});
 })
 
-export const deleteProduct = asyncHandler(async (req,res,next)=>{
-    const deleteProduct = await Product.findByIdAndDelete(req.params.productId);
-
-    if(!deleteProduct)
-        throw createError(404,`Product with id ${req.params.productId} not found `);
-
-    await deleteProduct.remove();
-    res.status(204).send({status : "success" , message : "Product deleted successfully"});
-})
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 import {Register,Login,UpdateDetails,UpdatePassword,ForgotPassword,ResetPassword} from '../controllers/AuthController.js';
-import {createProduct } from '../controllers/ProductController.js';
+import {CreateProduct , GetProducts , GetProduct, RemoveProduct, UpdateProduct } from '../controllers/ProductController.js';
 import express from 'express';
 const router = express.Router();
 router.route("/register").post(Register);
@@ -8,6 +8,10 @@ router.route("/update/userDetails").put(UpdateDetails);
 router.route("/update/password").put(UpdatePassword);
 router.route("/forgotPassword").post(ForgotPassword);
 router.route("/resetPassword").post(ResetPassword);
-router.route("/product").post(createProduct);
+router.route("/product").post(CreateProduct);
+router.route("/product").get(GetProducts);
+router.route("/product/:productId").get(GetProduct);
+router.route("/product/:productId").delete(RemoveProduct);
+router.route("/product/:productId").put(UpdateProduct);
 
 export default router;
